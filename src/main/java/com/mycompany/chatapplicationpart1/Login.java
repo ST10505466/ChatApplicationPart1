bc24eb9a1cbd9994715d18de039f135e89516c15
ST10505466 <ST10505466@myemeris.edu.za>
2026-04-14 17:07:22 +0200

    There was an error in the main class

---
+++ src/main/java/com/mycompany/chatapplicationpart1/Login.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.chatapplicationpart1;
+
+
+public class Login {
+
+    String registerUser(String username, String password, String number, String firstName, String lastName) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    boolean loginUser(String loginUser, String loginPass) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    boolean returnLoginStatus(boolean success) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
--- src/main/java/com/mycompany/chatapplicationpart1/login_java.java
+++
@@ -1,7 +0,0 @@
-
-package com.mycompany.chatapplicationpart1;
-
-
-public class login_java {
-    
-}
