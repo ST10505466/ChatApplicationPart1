b211a93cbbebd234fc6933000de5d7170d68318b
ST10505466 <ST10505466@myemeris.edu.za>
2026-06-25 21:50:14 +0200

    Add messagemanger with array class with array management

--- src/main/java/com/mycompany/chatapplicationpart1/MessageManager.java
+++ src/main/java/com/mycompany/chatapplicationpart1/MessageManager.java
@@ -7,13 +7,117 @@ package com.mycompany.chatapplicationpart1;
 
 public class MessageManager {
     //ARRAYS USED TO STORE MESSAGE INFORMATION
-    private String[] messageHashes = new String[100];
-    private String[] recipients = new String[100];
-    private String[] messages = new String[100];
+    private final String[] messageHashes = new String[100];
+    private final String[] recipients = new String[100];
+    private final String[] messages = new String[100];
     
     //TO KEEP TRACK OF THE AMOUNT OF MESSAGES THAT HAVE BEEN STORED
     private int messageCount = 0;
     
+   public void addMessages(String hash, String recipient, String message) {
+       //THIS METHOD STORES A NEW MESSGAE INSIDE THE ARRAYS
+       
+       //SAVE THE MESSAGE DETAILS
+       messageHashes[messageCount] = hash;
+       recipients[messageCount] = recipient;
+       messages[messageCount] = message;
+       
+       messageCount++;
+       //INCREASE THE COUNTER FOR THE NEXT MESSAGE
+   }
+   public String getLongMessage() {
+       //THIS METHOD SEARCHED THROUGH ALL THE MESSAGES AND RETURNS THE LONGEST MESSAGE THAT WAS STORED
+       
+       String longest = "";
+       //VARIABLE USED TO KEEP THE CURRENT LONGEST MESSAGE
+       
+       for (int i = 0; i < messageCount; i++) {
+           //LOOP THROUGH EVERY STORED MESSAGE
+           
+           if (messages[i].length() > longest.length()) {
+               //COMPARE MESSAGE LENGTHS
+               
+               longest = messages[i];
+               //UPDATE LONGEST MESSAGE
+           }
+       }
+       return longest;
+   }
+   
+   public String searchByRecipient(String recipient) {
+       //SEARCHES FOR ALL MESSAGES THAT BELONG TO THE RECIPIENT ENTERED BY THE USER
+       
+       String result = "";
+       //VARIABLE USED TO STORE ALL MATCHING MESSAGES
+       
+       for (int i = 0; i < messageCount; i++) {
+           //CHECK EVERY STORED RECIPIENT
+           
+           if (recipients[i].equals(recipient)) {
+               
+               result += messages[i] + "/n";
+               //ADDING MATCHING TO MATCH THE RESULTS
+               
+           }
+       }
+       if (result.isEmpty()) {
+           //IF NO MESSAGES WERE FOUND
+           
+           return"No messages found,";
+           
+       }
+       
+       return result;
+       
+   }
+   public String deleteMessage(String hash) {
+       //DELETES THE MESSAGES USISING MESSAGE HAS
+       //ONCE THE MESSAGE HAS BEEN DELETED THE REMAINING ARRAY VALUES WILL BE SHIFTED TO MAKE SURE THAT THERE ARE NOT ANY EMPTY SPACES
+       
+       for (int i = 0; i < messageCount; i++) {
+           //SEARCHING FOR THE MATCHING HASH
+           
+           if (messageHashes [i].equals(hash)) {
+               //STORE DELETED MESSAGE BEFORE REMOVING IT
+                              //STORE DELETED MESSAGE BEFORE REMOVING IT
+               
+               for (int j = i; j < messageCount - 1; j++) {
+                   //SHIFT ALL REMAINING ARRAY VALUES TO THE LEFFT
+                   
+                   messageHashes[j] = messageHashes[j + 1];
+                   recipients[j] = recipients[j + 1];
+                   messages[j] = messages[j + 1];
+                   
+               }
+               messageCount --;
+               //ONE LESS MESSAGE NOW
+               
+             
+               
+               
+           }
+       }
+       return "Message hash not found." ;
+       //IF THE MESSAGE HASH WAS NOT FOUND
+   }
+   
+   public String displayReport() {
+       //DISPLAYING A REPORT THAT SHOWS ALL THE STORED MESSAGES
+       
+       String report = "";
+       //VARIABLE THAT IS USED TO BUILD THE REPORT
+       
+       for (int i = 0; i < messageCount; i++) {
+           //LOOP THROUGH EVERY MESSAGE THAT IS STORED
+           
+           report += "Message Hash:  " + messageHashes[i] + "/n";
+           
+           
+       }
+       return report;
+       
+   }
+       
 
     
 }
